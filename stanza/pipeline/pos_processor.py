e38de3eb1673e07699fd15dc426a69a1775e8140
Giorgian Borca-Tasciuc <giorgian.borca-tasciuc@protonmail.com>
2021-10-10 12:42:42 -0400

    Added sentence-level next best

--- stanza/pipeline/pos_processor.py
+++ stanza/pipeline/pos_processor.py
@@ -76,7 +76,7 @@ class POSProcessor(UDProcessor):
             # Ignore XPOS score
             # TODO: figure out how to enable XPOS score
             self._scorer = lambda ups, xps, ufs: (ups + ufs) / 2
-
+            self._next_best = NextBest
         elif config.get('top_k_mode', 0) == 2:
             EQUIVALENCE_CLASSES = (
                     {'JJ', 'JJR', 'JJS'},
@@ -99,6 +99,53 @@ class POSProcessor(UDProcessor):
 
                 return len(xpos) # This indicates that there are no more options
             self._next_xpos = next_xpos
+            self._next_best = NextBest
+        elif config.get('top_k_mode', 0) == 3:
+            # TODO: these have to be verified
+            # Look at noun. Is it ok to have it a broad subset (all nouns, including proper)?
+            # or a strict one?
+            UPOS_MAPPINGS = {
+                    'ADJ': {'JJ', 'JJR', 'JJS'},
+                    'ADP': {'IN', 'RP'},
+                    'ADV': {'RB', 'RBR', 'RBS'},
+                    'AUX': {'MD'},
+                    'CCONJ': {'CC'},
+                    'DET': {'DT', 'PDT'},
+                    'INTJ': {'UH'},
+                    'NOUN': {'NN', 'NNS', 'NNP', 'NNPS'},
+                    'NUM': {'CD'},
+                    'PART': {'TO', 'POS'},
+                    'PRON': {'PRP', 'PRP$', 'WP', 'WDT', 'EX', 'WP$'},
+                    'PROPN': {'NNP', 'NNPS'},
+                    'PUNCT': {'.'},
+                    'SCONJ': {'IN', 'WRB'},
+                    'SYM': {'SYM'},
+                    'VERB': {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'},
+                    'X': {'FW', 'LS'}
+            }
+            self._next_best = NextBest
+            def next_xpos(upos, xpos, ufeats, upi, xpi, ufi, wi):
+                # get what would be the next upos tag
+                next_upos = self._next_upos(upos, xpos, ufeats, upi, xpi, ufi, wi)
+                # There's no more upos tags so we can't match
+                if next_upos >= len(upos):
+                    return len(xpos) # This indicates no more options
+
+                # this is the set of XPOS tags we'll accept
+                options_set = UPOS_MAPPINGS[upos[next_upos][0]]
+                # this is the set of XPOS tags we can actually use
+                xpos_options = tuple(filter(lambda x: x[1][0] in options_set, enumerate(xpos)))
+                # are there actually any tags?
+                if len(xpos_options) == 0:
+                    return len(xpos)
+
+                # return the highest-scoring XPOS tag that matches the UPOS tag
+                return xpos_options[0][0]
+            self._next_xpos = next_xpos
+            # Ignore XPOS score
+            # TODO: figure out how to enable XPOS score
+            self._scorer = lambda ups, xps, ufs: ups
+            self._next_best = MultiNextBest
 
         self._next_upos = config.get('next_upos', self._next_upos)
         self._next_xpos = config.get('next_xpos', self._next_xpos)
@@ -127,7 +174,7 @@ class POSProcessor(UDProcessor):
         docs = []
         scores = []
         serialized = batch.doc.to_serialized()
-        nb = NextBest(preds, self._scorer, self._next_upos, self._next_xpos, self._next_ufeats)
+        nb = self._next_best(preds, self._scorer, self._next_upos, self._next_xpos, self._next_ufeats)
         for score, pred in itertools.islice(nb, self._n_preds):
             copy = doc.Document.from_serialized(serialized)
             # pred should be (n_sent, n_word, n_feature)
@@ -165,7 +212,8 @@ class NextBest:
 
     def _access(self, feat, index, score=False):
         si = 1 if score else 0
-        return [[feat[index[i][j]][i][j][si] for j in range(len(index[i]))] for i in range(len(index))]
+        empty_feature = [None, 0.0]
+        return [[feat[index[i][j]][i][j][si] if index[i][j] >= 0 else empty_feature[si] for j in range(len(index[i]))] for i in range(len(index))]
 
     def _score(self, upos_index, xpos_index, ufeats_index):
         score = 0
@@ -213,26 +261,26 @@ class NextBest:
             n_words = len(self._preds[0][i])
             for j in range(n_words):
                 next_upos = self._next_wrapper(self._next_upos, upos_index_ret, xpos_index_ret, ufeats_index_ret, i, j)
-                if next_upos >= self._n_preds:
-                    continue
-
                 next_xpos = self._next_wrapper(self._next_xpos, upos_index_ret, xpos_index_ret, ufeats_index_ret, i, j)
-                if next_xpos >= self._n_preds:
-                    continue
-
                 next_ufeats = self._next_wrapper(self._next_ufeats, upos_index_ret, xpos_index_ret, ufeats_index_ret, i, j)
-                if next_ufeats >= self._n_preds:
-                    continue
 
-                new_upos_index = [list(sent) for sent in upos_index_ret]
-                new_upos_index[i][j] = next_upos
-                new_upos_index = tuple(tuple(sent) for sent in new_upos_index)
-                new_xpos_index = [list(sent) for sent in xpos_index_ret]
-                new_xpos_index[i][j] = next_xpos
-                new_xpos_index = tuple(tuple(sent) for sent in new_xpos_index)
-                new_ufeats_index = [list(sent) for sent in ufeats_index_ret]
-                new_ufeats_index[i][j] = next_ufeats
-                new_ufeats_index = tuple(tuple(sent) for sent in new_ufeats_index)
+                new_upos_index = upos_index_ret
+                new_xpos_index = xpos_index_ret
+                new_ufeats_index = ufeats_index_ret
+                if next_upos < self._n_preds:
+                    new_upos_index = [list(sent) for sent in upos_index_ret]
+                    new_upos_index[i][j] = next_upos
+                    new_upos_index = tuple(tuple(sent) for sent in new_upos_index)
+
+                if next_xpos < self._n_preds:
+                    new_xpos_index = [list(sent) for sent in xpos_index_ret]
+                    new_xpos_index[i][j] = next_xpos
+                    new_xpos_index = tuple(tuple(sent) for sent in new_xpos_index)
+
+                if next_ufeats < self._n_preds:
+                    new_ufeats_index = [list(sent) for sent in ufeats_index_ret]
+                    new_ufeats_index[i][j] = next_ufeats
+                    new_ufeats_index = tuple(tuple(sent) for sent in new_ufeats_index)
 
                 new_index = (new_upos_index, new_xpos_index, new_ufeats_index)
                 if new_index in self._seen:
@@ -243,6 +291,113 @@ class NextBest:
                 tc = self._tie_counter
                 self._tie_counter += 1
                 heapq.heappush(self._queue, (-new_score, tc, new_upos_index, new_xpos_index, new_ufeats_index))
+
+        upos_ret = self._access(self._upos, upos_index_ret)
+        xpos_ret = self._access(self._xpos, xpos_index_ret)
+        ufeats_ret = self._access(self._ufeats, ufeats_index_ret)
+
+        return -score_ret, self._pack_features(upos_ret, xpos_ret, ufeats_ret)
+
+class MultiNextBest:
+    def __init__(self, preds, scorer, next_upos, next_xpos, next_ufeats):
+        # preds: (n_pred, n_sentence, n_word, feature, score)
+        self._preds = preds
+        self._n_preds = len(self._preds)
+        self._n_sents = len(self._preds[0])
+        self._upos = self._extract_feature(preds, 0)
+        self._xpos = self._extract_feature(preds, 1)
+        self._ufeats = self._extract_feature(preds, 2)
+        self._scorer = scorer
+        self._next_upos = next_upos
+        self._next_xpos = next_xpos
+        self._next_ufeats = next_ufeats
+
+    @staticmethod
+    def _extract_feature(pred, fi):
+        return [[[pred[i][j][k][fi] for k in range(len(pred[i][j]))] for j in range(len(pred[i]))] for i in range(len(pred))]
+
+    @staticmethod
+    def _pack_features(upos, xpos, ufeats):
+        # Expects extracted upos, xpos, ufeats from _accesss
+        # Should NOT have score index
+        return [[(upos[i][j], xpos[i][j], ufeats[i][j]) for j in range(len(upos[i]))] for i in range(len(upos))]
+
+    def _access(self, feat, index, score=False):
+        si = 1 if score else 0
+        empty_feature = [None, 0.0]
+        return [[feat[index[i][j]][i][j][si] if index[i][j] >= 0 else empty_feature[si] for j in range(len(index[i]))] for i in range(len(index))]
+
+    def _score(self, upos_index, xpos_index, ufeats_index):
+        score = 0
+        total_lengths = 0
+        upos = self._access(self._upos, upos_index, score=True)
+        xpos = self._access(self._xpos, xpos_index, score=True)
+        ufeats = self._access(self._ufeats, ufeats_index, score=True)
+        feats = self._pack_features(upos, xpos, ufeats)
+        for sent in feats:
+            score += sum(map(lambda x: self._scorer(*x), sent))
+            total_lengths += len(sent)
+
+        score /= total_lengths
+
+        return score
+
+    def __iter__(self):
+        # What does an index look like?
+        # index is (n_sentence, n_word)
+        # Use simple summation to combine score across features
+        self._seen = set()
+        start_index = tuple(tuple(0 for i in range(len(sent))) for sent in self._upos[0])
+        score = self._score(start_index, start_index, start_index)
+        self._tie_counter = 0
+        tc = self._tie_counter
+        self._tie_counter += 1
+        self._queue = [(-score, tc, start_index, start_index, start_index)]
+        self._seen.add((start_index, start_index, start_index))
+
+        return self
+
+    def _next_wrapper(self, next_feat, upos_i, xpos_i, ufeats_i, i, j):
+        upos = [self._upos[k][i][j] for k in range(len(self._upos))]
+        xpos = [self._xpos[k][i][j] for k in range(len(self._upos))]
+        ufeats = [self._ufeats[k][i][j] for k in range(len(self._upos))]
+        return next_feat(upos, xpos, ufeats, upos_i[i][j], xpos_i[i][j], ufeats_i[i][j], (i, j))
+        
+
+    def __next__(self):
+        if len(self._queue) == 0:
+            raise StopIteration
+
+        score_ret, _, upos_index_ret, xpos_index_ret, ufeats_index_ret = heapq.heappop(self._queue)
+        for i in range(self._n_sents):
+            n_words = len(self._preds[0][i])
+            new_upos_index = [list(sent) for sent in upos_index_ret]
+            new_xpos_index = [list(sent) for sent in xpos_index_ret]
+            new_ufeats_index = [list(sent) for sent in ufeats_index_ret]
+            for j in range(n_words):
+                next_upos = self._next_wrapper(self._next_upos, upos_index_ret, xpos_index_ret, ufeats_index_ret, i, j)
+                next_xpos = self._next_wrapper(self._next_xpos, upos_index_ret, xpos_index_ret, ufeats_index_ret, i, j)
+                next_ufeats = self._next_wrapper(self._next_ufeats, upos_index_ret, xpos_index_ret, ufeats_index_ret, i, j)
+
+                if next_upos < self._n_preds:
+                    new_upos_index[i][j] = next_upos
+                if next_xpos < self._n_preds:
+                    new_xpos_index[i][j] = next_xpos
+                if next_ufeats < self._n_preds:
+                    new_ufeats_index[i][j] = next_ufeats
+
+            new_upos_index = tuple(tuple(sent) for sent in new_upos_index)
+            new_xpos_index = tuple(tuple(sent) for sent in new_xpos_index)
+            new_ufeats_index = tuple(tuple(sent) for sent in new_ufeats_index)
+            new_index = (new_upos_index, new_xpos_index, new_ufeats_index)
+            if new_index in self._seen:
+                continue
+
+            self._seen.add(new_index)
+            new_score = self._score(new_upos_index, new_xpos_index, new_ufeats_index)
+            tc = self._tie_counter
+            self._tie_counter += 1
+            heapq.heappush(self._queue, (-new_score, tc, new_upos_index, new_xpos_index, new_ufeats_index))
 
         upos_ret = self._access(self._upos, upos_index_ret)
         xpos_ret = self._access(self._xpos, xpos_index_ret)
